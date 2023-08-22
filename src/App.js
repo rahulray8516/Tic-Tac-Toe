6e4dac9b31b436682da8fa730573a4f0d2f562a1
rahulray8516 <ray.rahul.8101@gmail.com>
2023-08-22 16:28:08 +0530

    a

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return(
+  <div>
+      <div className="board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      </div>
+      <div className="board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      </div>
+      <div className="board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      </div>
+
+                  
+    </div>
+  );
 }
+
+ 
