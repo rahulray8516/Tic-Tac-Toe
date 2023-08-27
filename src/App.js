abc700fbd2a3790f6b9a73ebb79ad40f0190efd5
rahulray8516 <ray.rahul.8101@gmail.com>
2023-08-27 21:19:32 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
 import React from "react";
+import { useState } from "react";
+
+var x = 0;
+
+function Square(){
+
+  const [value,setValue] = useState(null);
+
+  function handleClick(){
+    x % 2 ? setValue('O') : setValue('X');
+    console.log("Clicked" + " " + ++x + " times")
+    console.log("Set State Value : " + )
+  }
+
+  return ( <button className="square" onClick={handleClick} >{value}</button> );
+} 
 
 
-export default function Square() {
+export default function Board() {
   return(
   <div>
     
       <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+       <Square  /><Square /><Square />
       </div>
       <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <Square  /><Square  /><Square />
       </div>
       <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <Square /><Square  /><Square />
       </div>
 
-        {/* Adding Zeroes */}
-
-      <div className="board-row">
-      <button className="square">O</button>
-      <button className="square">O</button>
-      <button className="square">O</button>
-      </div>
-      <div className="board-row">
-      <button className="square">O</button>
-      <button className="square">O</button>
-      <button className="square">O</button>
-      </div>
-      <div className="board-row">
-      <button className="square">O</button>
-      <button className="square">O</button>
-      <button className="square">O</button>
-      </div>
-  </div>
+   </div>
   );
 }
- 
